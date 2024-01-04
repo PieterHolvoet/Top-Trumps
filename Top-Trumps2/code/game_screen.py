5feaf2b2db1a0a9b8481e3268c77a0fc13a6974a
PieterPrograms <131905316+PieterHolvoet@users.noreply.github.com>
2024-01-04 12:33:25 +0100

    Nieuwe directory om te zien of deze wel voor iedereen werkt.

---
+++ Top-Trumps2/code/game_screen.py
@@ -0,0 +1,172 @@
+import pygame
+
+pygame.init()
+
+FPS = 60
+WIDTH = 800
+HEIGHT = 800
+
+# Minecraft Font :)
+FONT = pygame.font.Font('Top-Trumps2/assets/fonts/MinecraftRegular-Bmg3.otf', int(WIDTH // 40))
+MEDIUMFONT = pygame.font.Font('Top-Trumps2/assets/fonts/MinecraftRegular-Bmg3.otf', int(WIDTH // 17.4))
+GROOTFONT = pygame.font.Font('Top-Trumps2/assets/fonts/MinecraftRegular-Bmg3.otf', int(WIDTH // 10))
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+# Set up display
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Top Trumps")
+clock = pygame.time.Clock()
+
+startscreen = pygame.image.load("Top-Trumps2/assets/fotos/start_screen.jpg")
+startscreen = pygame.transform.scale(startscreen, (WIDTH, HEIGHT))
+
+background = pygame.image.load("Top-Trumps2/assets/fotos/background_topTrumps.JPG")
+background = pygame.transform.scale(background, (WIDTH, HEIGHT))
+
+ttFront = pygame.image.load('Top-Trumps2/assets/fotos/voorkant_kaart.jpg')
+ttFront = pygame.transform.scale(ttFront, (WIDTH // 3.333, HEIGHT // 2.5))
+
+ttBack = pygame.image.load('Top-Trumps2/assets/fotos/achterkant_kaart.jpg')
+ttBack = pygame.transform.rotate(ttBack, 180)
+ttBack = pygame.transform.scale(ttBack, (WIDTH // 3.333, HEIGHT // 2.5))
+
+
+def display_in_a_match(card, hoger_lager):
+    x1, x2 = WIDTH // 2.025, WIDTH // 1.616
+    ylist = [HEIGHT // 1.161, HEIGHT // 1.117, HEIGHT // 1.0796, HEIGHT // 1.0376]
+    screen.blit(background, (0, 0 - HEIGHT * 0.02))
+    screen.blit(ttFront, ((WIDTH // 2) - (ttFront.get_width() // 2), HEIGHT - ttFront.get_height()))
+    screen.blit(ttBack, ((WIDTH // 2) - (ttBack.get_width() // 2), 0))
+    image = pygame.image.load(f"Top-Trumps2/assets/fotos/animals_cards/{card.naam}.JPG")
+    image = pygame.transform.scale(image, (WIDTH // 4.4, HEIGHT // 6.7))
+    screen.blit(image, (WIDTH // 2.556, HEIGHT // 1.48))
+    for y in ylist:
+        pygame.draw.line(screen, WHITE, (x1, y), (x2, y))
+
+    if hoger_lager:
+        hoger_lager_text = "Hoger"
+    else:
+        hoger_lager_text = "Lager"
+    hoger_lager_text = MEDIUMFONT.render(hoger_lager_text, True, WHITE)
+    screen.blit(hoger_lager_text, (WIDTH * 0.8, HEIGHT * 0.06))
+
+    name_text = FONT.render(card.naam, True, (255, 255, 255))
+    screen.blit(name_text, (WIDTH // 2 - (WIDTH // 10.666), HEIGHT * 0.64))
+
+    for i in range(4):
+        attr_value = f"{card.attr_list[i]}"
+        attr_surface = FONT.render(attr_value, True, WHITE)
+        x_offset = len(attr_value) * WIDTH // 72.727
+        screen.blit(attr_surface,
+                    ((WIDTH // 2) + (WIDTH // 8) - x_offset, HEIGHT * (0.83 + (i * 0.035))))
+    pygame.display.flip()
+
+
+def won_lost_screen(player_kaart, com_kaart, gewonnen_verloren, player_aantal, com_aantal, hoger_lager, choice):
+    running = True
+    player_kaarten = f"{player_aantal}"
+    com_kaarten = f"{com_aantal}"
+    keuze = f"{choice}"
+    display_in_a_match(player_kaart, hoger_lager)
+    x1, x2, y1, y2 = WIDTH // 3.2, WIDTH // 1.454, HEIGHT // 2.424, HEIGHT // 1.7
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                if x1 <= mouse_x <= x2 and y1 <= mouse_y <= y2:
+                    running = False
+        screen.blit(ttFront, ((WIDTH / 2) - (ttBack.get_width() / 2), 0))
+
+        name_text = FONT.render(com_kaart.naam, True, WHITE)
+        screen.blit(name_text, (WIDTH // 2 - (WIDTH // 10.666), HEIGHT * 0.04))
+        image = pygame.image.load(f"Top-Trumps2/assets/fotos/animals_cards/{com_kaart.naam}.JPG")
+        image = pygame.transform.scale(image, (WIDTH // 4.4, HEIGHT // 6.7))
+        screen.blit(image, (WIDTH // 2.556, HEIGHT // 14.545))
+        for i in range(4):
+            attr_value = f"{com_kaart.attr_list[i]}"
+            x_offset = len(attr_value) * WIDTH // 72.727
+            attr_surface = FONT.render(attr_value, True, WHITE)
+            screen.blit(attr_surface,
+                        ((WIDTH // 2) + (WIDTH // 8) - x_offset, HEIGHT * (0.23 + (i * 0.035))))
+
+        if gewonnen_verloren == 1:
+            gewonnen_verloren_txt = "Gewonnen"
+            gewonnen_verloren_txt = MEDIUMFONT.render(gewonnen_verloren_txt, True, GREEN)
+        elif gewonnen_verloren == -1:
+            gewonnen_verloren_txt = "Verloren"
+            gewonnen_verloren_txt = MEDIUMFONT.render(gewonnen_verloren_txt, True, RED)
+        else:
+            gewonnen_verloren_txt = "Gelijk"
+            gewonnen_verloren_txt = MEDIUMFONT.render(gewonnen_verloren_txt, True, YELLOW)
+        next = GROOTFONT.render("Verder", True, WHITE)
+        screen.blit(next, (WIDTH // 2 - WIDTH // 6.145, HEIGHT // 2 - HEIGHT // 20))
+        screen.blit(gewonnen_verloren_txt, (WIDTH * 0.72, HEIGHT * 0.01))
+        player_aantal = FONT.render(player_kaarten, True, WHITE)
+        com_aantal = FONT.render(com_kaarten, True, WHITE)
+        screen.blit(com_aantal, ((WIDTH // 2) + (WIDTH // 15), HEIGHT * 0.025))
+        screen.blit(player_aantal, ((WIDTH // 2) + (WIDTH // 15), HEIGHT * 0.625))
+        keuzeTekst = GROOTFONT.render(keuze, True, WHITE)
+        screen.blit(keuzeTekst, (WIDTH * 0.86, HEIGHT // 2 - HEIGHT // 20))
+        pygame.display.flip()
+
+
+# def start_screen():
+#     x1, x2 = sc.get_width() // 2.8, sc.get_width() // 1.5
+#     y1, y2 = sc.get_height() // 1.5625, sc.get_height() // 1.4
+#     running = True
+#     while running:
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 running = False
+#             elif event.type == pygame.KEYDOWN:
+#                 if event.key == pygame.K_RETURN:
+#                     running = False
+#                     game_loop()  # Call the game loop function when Enter key is pressed
+#             elif event.type == pygame.MOUSEBUTTONDOWN:
+#                 mouse_x, mouse_y = pygame.mouse.get_pos()
+#                 print(mouse_x)
+#                 print(mouse_y)
+#                 print()
+#                 if x1 <= mouse_x <= x2 and y1 <= mouse_y <= y2:
+#                     running = False
+#                     game_loop()
+#         # Draw background
+#         sc.blit(startscreen, (0, 0))
+#
+#         # Update the display
+#         pygame.display.flip()
+#
+#         # Cap the frame rate
+#         pygame.time.Clock().tick(FPS)
+
+
+def timer_clock(timer_seconds, kaart, hoger_lager):
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        # sc.fill(BLACK)
+        display_in_a_match(kaart, hoger_lager)
+        timer_text = GROOTFONT.render(str(timer_seconds), True, WHITE)
+        text_rect = timer_text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+        screen.blit(timer_text, text_rect)
+
+        pygame.display.flip()
+        clock.tick(1)
+        timer_seconds -= 1
+
+        if timer_seconds < 0:
+            running = False
---
+++ Top-Trumps2/code/kaart.py
@@ -0,0 +1,70 @@
+import csv
+
+
+class Kaart:
+    def __init__(self, naam, attr_list):
+        self.naam = naam
+        self.attr_list = attr_list
+
+    def isgelijk(self, other, attr):
+        if attr == 1:
+            return self.attr_list[0] == other.attr_list[0]
+        elif attr == 2:
+            return self.attr_list[1] == other.attr_list[1]
+        elif attr == 3:
+            return self.attr_list[2] == other.attr_list[2]
+        elif attr == 4:
+            return self.attr_list[3] == other.attr_list[3]
+
+    def isgroter(self, other, attr):
+        if attr == 1:
+            return self.attr_list[0] > other.attr_list[0]
+        elif attr == 2:
+            return self.attr_list[1] > other.attr_list[1]
+        elif attr == 3:
+            return self.attr_list[2] > other.attr_list[2]
+        elif attr == 4:
+            return self.attr_list[3] > other.attr_list[3]
+
+
+# Is niet meer nodig wegens uit database halen.     (maar was simpel en beter om hier te doen, klein algoritme)
+# PS:: Moet nog gefixt worden
+#
+DECK_DIEREN_CSV = []
+with open('Top-Trumps2/assets/Dieren_TopTrumps.csv', 'r') as csv_bestand:
+    csv_lezer = csv.reader(csv_bestand)
+    header = next(csv_lezer)
+    attr1, attr2, attr3, attr4 = header[1], header[2], header[3], header[4]
+    for rij in csv_lezer:
+        attr_lijst = [float(rij[1]), float(rij[2]), float(rij[3]), float(rij[4])]
+        dier = Kaart(rij[0], attr_lijst)
+        DECK_DIEREN_CSV.append(dier)
+
+sorted_attr1 = []
+sorted_attr2 = []
+sorted_attr3 = []
+sorted_attr4 = []
+
+for kaart in DECK_DIEREN_CSV:
+    sorted_attr1.append(kaart.attr_list[0])
+    sorted_attr2.append(kaart.attr_list[1])
+    sorted_attr3.append(kaart.attr_list[2])
+    sorted_attr4.append(kaart.attr_list[3])
+sorted_attr1 = sorted(sorted_attr1)
+sorted_attr2 = sorted(sorted_attr2)
+sorted_attr3 = sorted(sorted_attr3)
+sorted_attr4 = sorted(sorted_attr4)
+
+
+def rank_kaart_attr(kaart, hoger_lager):
+    waarde1, waarde2, waarde3, waarde4 = kaart.attr_list[0], kaart.attr_list[1], kaart.attr_list[2], kaart.attr_list[3]
+    rank1 = sorted_attr1.index(waarde1)
+    rank2 = sorted_attr2.index(waarde2)
+    rank3 = sorted_attr3.index(waarde3)
+    rank4 = sorted_attr4.index(waarde4)
+    rank_list = [rank1, rank2, rank3, rank4]
+    if hoger_lager:  # True==Hoger
+        wanted_rank = max(rank_list)
+    else:
+        wanted_rank = min(rank_list)
+    return rank_list.index(wanted_rank) + 1
